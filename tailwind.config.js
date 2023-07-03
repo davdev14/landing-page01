df5b2d26634a6b786dd8167ceaced5f035b148aa
davdev14 <davdev1400@gmail.com>
2023-07-03 18:44:24 +0100

    nav done

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    screens: {
+      'xs': '375px',
+    },
+    extend: {
+      screens:{
+        'sm': '375px'
+      },
+    },
+  },
+  plugins: [],
+}
+
